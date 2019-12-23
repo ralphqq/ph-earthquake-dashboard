639bab5eb75fa2899e59e086243bd3c4a765314e
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-12-23 16:53:32 +0800

    Initial commit

---
+++ ph_earthquakes/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ph_earthquakes project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ph_earthquakes.settings')
+
+application = get_wsgi_application()
