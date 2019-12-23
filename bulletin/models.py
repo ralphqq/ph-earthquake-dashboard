996d76deb793a42bb7009cc5b43ce9dfa06f53e6
Ralph Quirequire <ralph.quirequire@outlook.com>
2019-12-23 19:08:48 +0800

    Define Bulletin model

---
+++ bulletin/models.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from django.db import models
+import pytz
+
+
+class Bulletin(models.Model):
+    time_of_quake = models.DateTimeField(
+        default=datetime(1900, 1, 1, tzinfo=pytz.UTC),
+        null=False
+    )
+    url = models.URLField(null=False, unique=True)
+    latitude = models.DecimalField(max_digits=5, decimal_places=2)
+    longitude = models.DecimalField(max_digits=5, decimal_places=2)
+    depth = models.DecimalField(max_digits=4, decimal_places=1)
+    magnitude = models.DecimalField(max_digits=3, decimal_places=1)
+    location = models.CharField(max_length=2048, default='')
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['time_of_quake']),
+            models.Index(fields=['url'])
+        ]
+        ordering = ['-time_of_quake']
+    def __str__(self):
+        return self.url
--- ph_earthquakes/settings.py
+++ ph_earthquakes/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'bulletin',
     'django.contrib.staticfiles',
 ]
 
@@ -105,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Manila'
 
 USE_I18N = True
 
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
