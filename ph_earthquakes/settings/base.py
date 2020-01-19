1214160f829631d77cbc8be1b964b2b01c0cae2b
Ralph Quirequire <ralph.quirequire@outlook.com>
2020-01-19 10:35:27 +0800

    Configure and migrate to Postgres

--- ph_earthquakes/settings/base.py
+++ ph_earthquakes/settings/base.py
@@ -72,8 +72,15 @@ WSGI_APPLICATION = 'ph_earthquakes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST', ''),
+        'PORT': os.environ.get('DB_PORT', '5432'),
+        'TEST': {
+            'NAME': 'test_quakes_db'
+        },
     }
 }
 
