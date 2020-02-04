36074d9b8e8e6edd7d6d6b82f30f296837356984
Ralph Quirequire <ralph.quirequire@outlook.com>
2020-02-04 14:14:18 +0800

    Apply factory on test_models

--- bulletin/tests/test_models.py
+++ bulletin/tests/test_models.py
@@ -1,23 +1,21 @@
 from django.db.utils import IntegrityError
 from django.test import TestCase
 
+from bulletin.factories import BulletinFactory
 from bulletin.models import Bulletin
 
 
 class BulletinModelTest(TestCase):
 
     def test_completing_required_fields(self):
-        b = Bulletin(
-            url='https://earthquake.phivolcs.gov.ph/',
-            latitude=6.9,
-            longitude=125.2,
-            depth=35,
-            magnitude=1.9
-        )
+        b = BulletinFactory()
         b.save()
         self.assertEqual(Bulletin.objects.count(), 1)
 
     def test_violating_not_null_fields(self):
         with self.assertRaises(IntegrityError):
-            b = Bulletin()
+            b = BulletinFactory(
+                time_of_quake=None,
+                url=None
+            )
             b.save()
